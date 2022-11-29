cf13b05d98a84c3b8fffaaf00206ec0ee4c58d65
syed muhammad bilal <syedmuhammadbilal638@gmail.com>
2022-11-29 18:04:06 +0500

    some changes has been completed

--- src/App.js
+++ src/App.js
@@ -1,38 +1,93 @@
-import "./App.css";
-import React from "react";
-import FIRSTDIV from "./all-compounent/header/index";
-import IMop from "./all-compounent/images/image";
-import Banner_bg from "./all-compounent/images";
+import React from 'react'
+// import Topbar from "../src/components/Topbar";
+import Topbar from "./components/Topbar";
+import Header from "./components/Header";
+import Mobilenav from "./components/Mobilenav";
+import Search from './components/Search';
+import Slider1 from './components/Slider1';
+import Slider2 from './components/Slider2';
+import Slider3 from './components/Slider3';
+import Section4 from './components/Section4';
+import Slider4 from './components/Slider4';
+import Slider5 from './components/Slider5';
+import Faq from './components/Faq';
+import Footer from './components/Footer';
 
-function App() {
+
+
+const Home = () => {
   return (
-    <div className="conatiner">
-      <div className="firstback">
-        <h5>
-          <FIRSTDIV />
-        </h5>
-      </div>
+    <>
+    <Topbar />
+    <Header />
+    <Mobilenav/>
 
-      <div className="">
-        <div className="image">
-          <IMop />
-        </div>
+    {/* BANNER START */}
 
-
-      </div>
-
-      <div className="banner-bg">
-        <div className="conatiner">
-          <div className="row">
-            <div className="col">
-
+    <section className='banner_bg'>
+            <div className="container">
+                <div className="row">
+                    <div className="col">
+                        <Search />
+                    </div>
+                </div>
             </div>
-          </div>
-        </div>
+    </section>  
 
-      </div>
-    </div>
-  );
+    {/* BANNER END */}
+
+
+    <Slider1/>
+    <Slider2/>
+    <Slider3/>
+    <Section4/>
+    <Slider4/>
+    <Slider5/>
+    <Faq/>
+    <Footer/>
+    </>
+  )
 }
 
-export default App;
+
+export default Home;
+
+
+// import "./App.css";
+// import React from "react";
+// import FIRSTDIV from "./all-compounent/header/index";
+// import IMop from "./all-compounent/images/image";
+// // import Banner_bg from "./all-compounent/images";
+
+// function App() {
+//   return (
+//     <div className="conatiner">
+//       <div className="firstback">
+//         <h5>
+//           <FIRSTDIV />
+//         </h5>
+//       </div>
+
+//       <div className="">
+//         <div className="image">
+//           <IMop />
+//         </div>
+
+
+//       </div>
+
+//       <div className="banner-bg">
+//         <div className="conatiner">
+//           <div className="row">
+//             <div className="col">
+
+//             </div>
+//           </div>
+//         </div>
+
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default App;
