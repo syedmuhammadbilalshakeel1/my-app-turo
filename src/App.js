4681e0b0bb1e6829746de432fc5f4c31208210df
syed muhammad bilal <syedmuhammadbilal638@gmail.com>
2022-11-29 18:17:26 +0500

    not completed yet

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import Slider4 from './components/Slider4';
 import Slider5 from './components/Slider5';
 import Faq from './components/Faq';
 import Footer from './components/Footer';
+import "App.css"
 
 
 
@@ -52,42 +53,3 @@ const Home = () => {
 
 export default Home;
 
-
-// import "./App.css";
-// import React from "react";
-// import FIRSTDIV from "./all-compounent/header/index";
-// import IMop from "./all-compounent/images/image";
-// // import Banner_bg from "./all-compounent/images";
-
-// function App() {
-//   return (
-//     <div className="conatiner">
-//       <div className="firstback">
-//         <h5>
-//           <FIRSTDIV />
-//         </h5>
-//       </div>
-
-//       <div className="">
-//         <div className="image">
-//           <IMop />
-//         </div>
-
-
-//       </div>
-
-//       <div className="banner-bg">
-//         <div className="conatiner">
-//           <div className="row">
-//             <div className="col">
-
-//             </div>
-//           </div>
-//         </div>
-
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default App;
